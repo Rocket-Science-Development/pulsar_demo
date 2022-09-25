e872ec31d332c404061aa36693858913003b0e53
michael <michael.makashyn@oak-tree.us>
2022-09-25 20:58:11 +0200

    Made changes to docker, and prepared graphana dashboard

--- Dockerfile
+++ Dockerfile
@@ -15,6 +15,7 @@ WORKDIR /app
 
 COPY requirements.txt /app/requirements.txt
 COPY /src/compute_metrics_script.py /app/compute_metrics_script.py
+COPY /datasets/ /app/datasets
 
 RUN pip install --upgrade pip && pip install --no-cache-dir -r requirements.txt
 
--- src/compute_metrics_script.py
+++ src/compute_metrics_script.py
@@ -1,1 +1,69 @@
-print("hello world")
+import uuid
+import logging
+import pandas as pd
+from datetime import datetime
+from pulsar_data_collection.data_capture import DataCapture, DatabaseLogin, DataWithPrediction
+from pulsar_metrics.analyzers.base import Analyzer
+
+logger = logging.getLogger("__main__")
+
+if __name__ == "__main__":
+    # Reading reference dataset
+    df_result = pd.DataFrame()
+    df_ref = pd.read_csv('/app/datasets/kidney_test.csv')
+
+    df_ref['model_id'] = '1'
+    df_ref['model_version'] = '2'
+    df_ref['pred_timestamp'] = datetime.strptime('10/7/2022 22:30:30', '%d/%m/%Y %H:%M:%S')
+    df_ref['period'] = 'reference'
+
+
+    # Reading the newest prediction data
+    database_login = DatabaseLogin(db_host="influxdb", db_port=8086, db_user="admin", db_password="pass123", protocol="line")
+
+    dat_capture = DataCapture(
+        storage_engine="influxdb",
+        operation_type="METRICS",
+        login_url=database_login
+    )
+
+    # receiving the last period
+    last_eval_timestamp = dat_capture.collect_eval_timestamp()
+    if last_eval_timestamp:
+        last_eval_timestamp_str = last_eval_timestamp.strftime('%Y-%m-%d %H:%M:%S')
+        db_df = pd.DataFrame(dat_capture.collect({"time": f">= '{last_eval_timestamp_str}'"}).get("prediction"))
+    else:
+        db_df = pd.DataFrame(dat_capture.collect().get("prediction"))
+
+    logger.info(f"Dataframe collected, df length: {len(db_df)}")
+
+    if len(db_df):
+        # TODO: it should be changed, it's not clear why we cannot use datetime now
+        db_df['pred_timestamp'] = datetime.strptime('20/8/2022 22:30:30', '%d/%m/%Y %H:%M:%S')
+
+        analysis = Analyzer(name='Compute Script Analyzer', description='Analyzer for compute script usage', data=db_df)
+        analysis.add_drift_metrics(
+            metrics_list=['wasserstein', 'ttest', 'ks_2samp','kl','manwu','levene','bftest','CvM'],
+            features_list=['age','al','ane','appet','ba','bgr','bp','bu','cad','dm','hemo','htn','id','pc','pcc','pcv','pe','pot','rbc','rbcc','sg','sc','sod','su']
+        )
+
+        analysis.run(data_ref=df_ref, options={'ttest': {'alpha': 0.05, 'equal_var': False}})
+        df_result_drift = analysis.results_to_pandas()
+
+        df_result_drift.set_index("eval_timestamp", inplace=True)
+
+        df_result_drift["uuid"] = [uuid.uuid4() for _ in range(len(df_result_drift.index))]
+        df_result_drift["period_start"] = df_result_drift["period_start"].dt.strftime('%Y-%m-%dT%H:%M:%SZ')
+        df_result_drift["period_end"] = df_result_drift["period_end"].dt.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+        dat_capture.push_metrics(df_result_drift)
+
+        logger.info(f"Metrics pushed to the db")
+
+        # Add the last period to db after pushing
+        eval_timestamp_df = pd.DataFrame({"uuid": uuid.uuid4(), "timestamp": datetime.now(),
+                                          "eval_timestamp": datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')}, index=[0, ])
+        eval_timestamp_df.set_index("timestamp", inplace=True)
+        dat_capture.push_eval_timestamp(eval_timestamp_df)
+        logger.info(f"Eval timestamp is updated in the db. Timestamp is: {eval_timestamp_df}")
+
