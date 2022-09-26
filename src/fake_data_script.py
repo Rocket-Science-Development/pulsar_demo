28c1adf36380d9037600d742a5150424158e3002
michael <michael.makashyn@oak-tree.us>
2022-09-26 15:40:52 +0200

    Created script for adding metrics with fake data

--- src/fake_data_script.py
+++ src/fake_data_script.py
@@ -1,16 +1,37 @@
 import pandas as pd
 import pickle as pkl
-from pydantic import BaseModel, validator
-import numpy as np
-from datetime import datetime
-from enum import Enum
-from collections.abc import Iterable
-from functools import partial
-import matplotlib.pyplot as plt
-import pytz
 from pulsar_data_collection.data_capture import DataCapture, DatabaseLogin, DataWithPrediction
 
 
-data_test = pd.read_csv('/app/datasets/test_data_no_class.csv')
+if __name__ == "__main__":
+    print("Pushing data")
+    data_test = pd.read_csv('/app/datasets/test_data_no_class.csv')
 
-model = pkl.load(open("/app/models/kidney_disease.pkl", "rb"))
+    model = pkl.load(open("/app/models/kidney_disease.pkl", "rb"))
+
+    test_data = data_test.sample(frac=0.3, random_state=1)
+
+    prediction = model.predict(test_data)
+
+    database_login = DatabaseLogin(
+        db_host="influxdb",
+        db_port=8086,
+        db_user="admin",
+        db_password="pass123",
+        protocol="line"
+    )
+
+    dat_capture = DataCapture(
+        storage_engine="influxdb",
+        model_id="1",
+        model_version="2",
+        data_id="FluxDB",
+        y_name="y_pred",
+        pred_name="clf_target",
+        operation_type="INSERT_PREDICTION",
+        login_url=database_login
+    )
+
+    dat_predict = DataWithPrediction(prediction=prediction, data_points=test_data)
+
+    dat_capture.push(dat_predict)
