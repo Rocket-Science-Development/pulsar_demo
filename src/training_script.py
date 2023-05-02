7753de2e542f5730b2c9a0c2e0a93a81e506b833
manuarya27 <107576853+manuarya27@users.noreply.github.com>
2023-05-02 00:10:32 -0400

    Adding comment on model.fit()
    model.train dataframe and target atributes explained in the comments.

--- src/training_script.py
+++ src/training_script.py
@@ -27,6 +27,10 @@ class Classifier:
                  # handle any PickleError exceptions that may occur during serialization
                  print("Error occurred while pickling:", e)
 
+    """
+    Trains a machine learning model using a dataframe (self. df_train and specified features [self.num_features + self.cat_features] 
+    and target (self.target) attributes.
+    """
     def train(self):
         self.model = CatBoostClassifier(iterations=2,
                                    learning_rate=1,
