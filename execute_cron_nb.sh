f2ca5285abf18ebf0b34974a90aebac1976f043d
Manu Arya <manuarya@Manus-MacBook-Air.local>
2023-07-31 10:48:08 -0400

    PUL-336 Commiting code for microservice segregation

--- execute_cron_nb.sh
+++ execute_cron_nb.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+RUN pip install jupyterlab
+
+# Expose the port on which Jupyter Notebook will run
+EXPOSE 8888
 #Running jupyter lab
 jupyter lab --ip 0.0.0.0 --allow-root --no-browser
 
+# Expose the port that the FastAPI application will run
+EXPOSE 8000
 #Running FastAPI
 uvicorn main:app --reload --host 0.0.0.0 --port 8000
